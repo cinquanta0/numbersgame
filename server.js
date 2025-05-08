a919f6582e4f35146e1f2f71dbf4d6915fd85552
cinquanta0 <135871626+cinquanta0@users.noreply.github.com>
2025-05-08 11:59:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,62 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-let waitingPlayer = null;
+const rooms = {};
 
 io.on('connection', (socket) => {
-  console.log(`Giocatore connesso: ${socket.id}`);
+  console.log('Un giocatore si è connesso:', socket.id);
 
-  if (waitingPlayer) {
-    // Abbina due giocatori
-    const room = `room-${waitingPlayer.id}-${socket.id}`;
+  socket.on('join_game', (playerName) => {
+    // Trova o crea una stanza con 1 solo giocatore
+    let room = Object.keys(rooms).find(r => rooms[r].length === 1);
+    if (!room) {
+      room = `room_${socket.id}`;
+      rooms[room] = [];
+    }
+
+    rooms[room].push({ id: socket.id, name: playerName });
     socket.join(room);
-    waitingPlayer.join(room);
 
-    io.to(room).emit('startMatch', { room });
-    waitingPlayer = null;
-  } else {
-    // Attendi un secondo giocatore
-    waitingPlayer = socket;
-    socket.emit('waiting', 'In attesa di un avversario...');
-  }
+    console.log(`${playerName} è entrato nella ${room}`);
 
-  socket.on('guessNumber', (data) => {
-    // Invia il numero all'altro giocatore
-    socket.to(data.room).emit('opponentGuess', data.guess);
+    if (rooms[room].length === 2) {
+      // Inizia la sfida
+      const secretNumber = Math.floor(Math.random() * 100) + 1;
+      io.to(room).emit('game_start', {
+        room,
+        players: rooms[room].map(p => p.name),
+        number: secretNumber
+      });
+    }
+  });
+
+  socket.on('guess', ({ room, guess }) => {
+    const target = parseInt(rooms[room]?.target);
+    if (!target) return;
+
+    if (guess === target) {
+      io.to(room).emit('game_result', {
+        winner: socket.id
+      });
+      delete rooms[room]; // reset room
+    } else {
+      socket.emit('guess_feedback', guess < target ? 'Troppo basso' : 'Troppo alto');
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log(`Giocatore disconnesso: ${socket.id}`);
-    if (waitingPlayer && waitingPlayer.id === socket.id) {
-      waitingPlayer = null;
+    for (const room in rooms) {
+      rooms[room] = rooms[room].filter(p => p.id !== socket.id);
+      if (rooms[room].length === 0) delete rooms[room];
     }
   });
 });
 
 server.listen(3000, () => {
-  console.log('Server attivo sulla porta 3000');
+  console.log('Server multiplayer attivo su http://localhost:3000');
 });
